0fcdafdf46fcf1ae6c9d3196e14b95890e6c4cf1
cybernico14 <nikissue@gmail.com>
2024-05-04 11:33:34 +0200

    "first commit of my datingApp"

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+ private readonly DataContext _context;
+
+ public UsersController(DataContext context)
+ {
+  _context = context;
+ }   
+ [HttpGet]
+ public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+ {
+  var users= await _context.Users.ToListAsync();
+  return users;
+ }
+
+ [HttpGet("{id}")]
+ public ActionResult<AppUser> GetUser(int id)
+ {
+  var user= _context.Users.Find(id);
+  return user;
+ }
+}
