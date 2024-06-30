0e785e88295cd1bbefd465308ecdf86767b9d2de
Nicolò Ferronato <nicolo.ferronato@pearson.com>
2024-06-30 11:38:08 +0200

    added features end of section 7

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountService = inject(AccountService);
+  const toastr = inject(ToastrService);
+  if (accountService.currentUser()) {
+    return true;
+  } else {
+    toastr.error('You are not authorized to access this page');
+    return false;
+  }
+};
